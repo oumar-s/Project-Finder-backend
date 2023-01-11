85c9080da5bd45db32c66274634ecba096ab5fe2
Oumar Siby <oumarsiby10@gmail.com>
2023-01-11 06:54:02 -0500

    Finished  setting up the environment

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
